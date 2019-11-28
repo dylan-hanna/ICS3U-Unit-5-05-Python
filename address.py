7af5a5e1b958227daf68c2753246e23d4a75f458
Ubuntu <ubuntu@ip-172-31-29-44.ec2.internal>
2019-11-28 18:08:00 +0000

    Turned user's address and info into canada post format

---
+++ address.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Created by: Dylan Hanna
+# Created on: Nov 2019
+# This program accepts user address information
+
+def mailing(name_one, address_one, city_one, province_one, postal_code_one):
+    print(name_one)
+    print(address_one)
+    print(city_one, province_one, postal_code_one)
+
+
+def main():
+    while True:
+        name = input("Enter the name of the reciever: ")
+        address = input("Enter the address: ")
+        city = input("Enter the city: ")
+        province = input("Enter the province: ")
+        postal_code = input("Enter the postal code: ")
+        print()
+
+        try:
+            mailing(name, address, city, province, postal_code)
+        except ValueError:
+            print("Invalid Input")
+            continue
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
